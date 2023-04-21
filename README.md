fed8abb582a6363070318c96fe2a1db51f5363c9
Whynotwynaut <125845043+EHernandez1213@users.noreply.github.com>
2023-04-20 22:09:25 -0700

    Added date to header and to readme file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Scheduler
+
+## Description
+
+This is a daily planner to keep track of tasks that need to be done throughout the day.
+
+## Installation
+
+N/A
+
+# Usage
+
+When the user types a task and hits the save button on the right of the time slot, the task will be stored. This task will be saved in local storage so the user can close the tab and reopen it with the tasks still there. The blocks that are green are hours that have already passed. The red block is the current hour. The gray blocks are future hours. The planner also tells you the current week day and todays date above the tasks.
+
+## License 
+
+The MIT License (MIT)
+
+Copyright (c) 2023 Elenilson Hernandez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   
   $('.saveBtn').on('click',saveItem)
 
@@ -19,9 +12,9 @@ $(function () {
   var time= ($(this).parent().attr('id'));
   localStorage.setItem(time, text);
   }
-
+// saves text to local storage
   
-  $('#hour-0 .description').val(localStorage.getItem('hour-0'));
+  
   $('#hour-1 .description').val(localStorage.getItem('hour-1'));
   $('#hour-2 .description').val(localStorage.getItem('hour-2'));
   $('#hour-3 .description').val(localStorage.getItem('hour-3'));
@@ -45,14 +38,13 @@ $(function () {
   $('#hour-21 .description').val(localStorage.getItem('hour-21'));
   $('#hour-22 .description').val(localStorage.getItem('hour-22'));
   $('#hour-23 .description').val(localStorage.getItem('hour-23'));
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  $('#hour-24 .description').val(localStorage.getItem('hour-24'));
+  // retrieves text from local storage
+
   $('.time-block').each(function (){
     var now = dayjs().hour();
-    var scheduleHour = parseInt($(this).parent().attr('id').split('-')[1]);
+    var scheduleHour = parseInt($(this).attr('id').split('-')[1]);
+    console.log(scheduleHour);
     if(now < scheduleHour){
       $(this).addClass('past');
     } else if (now === scheduleHour) {
@@ -61,12 +53,9 @@ $(function () {
       $(this).addClass('future');
     }
   })
+  // color codes the blocks based of what time it is
   
-  
-  // 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var today = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(today);
+//  dispays today's date
 });
